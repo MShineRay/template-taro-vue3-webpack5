8e3f4c29dcafa5561dd3840988d6ad9937ae420f
msr <mshineray@qq.com>
2025-04-03 09:54:21 +0800

    feat: init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">2222
+    <Counter />222
+  </view>
+</template>
+
+<script>
+import './index.scss'
+import Counter from '../../components/Counter.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    Counter
+  }
+}
+</script>
